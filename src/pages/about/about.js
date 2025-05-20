945f05d26b3340fb420a396c3960af4d47cd4ff6
zacdeng <zacdeng0720@gmail.com>
2025-05-20 13:47:57 -0700

    update equipment && css style

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -14,21 +14,22 @@ function About() {
           <div className="links">
             <p>
               <img src="https://i.loli.net/2020/07/19/B4sGYjqAmCTVxgb.png" width="20px" alt="email"/> 
-              <a href="mailto:zacdeng0720@email.com">zacdeng0720@gmail.com</a>
+              <a href="mailto:zacdeng0720@email.com"><i>zacdeng0720@gmail.com</i></a>
             </p>
             <p>
               <img src="https://s2.loli.net/2022/06/03/Orq1ki4lMd2bDyo.png" width="20px" alt="ig"/> 
-              <a href="https://www.instagram.com/zacdeng.film/">@zacdeng.film</a>
+              <a href="https://www.instagram.com/zacdeng.film/"><i>@zacdeng.film</i></a>
             </p>
             <p>
               <img src="https://s2.loli.net/2025/04/20/nDgN7pZmxetS1PQ.png" width="20px" alt="rednotes"/> 
-              <a href="https://www.xiaohongshu.com/user/profile/5df4a5420000000001001e59">@瞎按快門</a>
+              <a href="https://www.xiaohongshu.com/user/profile/5df4a5420000000001001e59"><i>@瞎按快門</i></a>
             </p>
           </div>
           
           <p className="equipment"><b>EQUIPMENTS</b></p>
-          <p><img src="https://s2.loli.net/2025/04/28/VWNFnxY5d12eBkH.png" width="25px" alt="rollei" />Rollei 35 s</p>
+          <p><img src="https://s2.loli.net/2025/04/28/VWNFnxY5d12eBkH.png" width="25px" alt="rollei" />Rollei 35s</p>
           <p><img src="https://s2.loli.net/2025/04/28/Zt2VX9ieAgcUEoO.png" width="25px" alt="bronica" />Bronica SQA</p>
+          <p><img src="https://s2.loli.net/2025/05/21/9lO3dvGxmoj4Mzq.png" width="25px" alt="bronica" />Mamiya 645 1000s</p>
           <p><img src="https://s2.loli.net/2025/04/28/NrCFDVEMRijPW8S.png" width="25px" alt="fujifilm" />Fujifilm xs20</p>
           <p><img src="https://s2.loli.net/2025/04/28/zKGpHCcJMLnaTfD.png" width="25px" alt="instant" />Fujifilm Instax Wide 300 / Mini 90</p>
         </div>
